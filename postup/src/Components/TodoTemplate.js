f4f7b039fa62ceef10fa0a99b1a999954d81874c
asd <asd>
2019-08-08 16:52:37 +0900

    Change TodoTemplate

--- postup/src/Components/TodoTemplate.js
+++ postup/src/Components/TodoTemplate.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled,{css} from 'styled-components';
 import '../Css/TodoTemplateCss.css';
-import {MdSchedule} from 'react-icons/md';
+import {MdAdd} from 'react-icons/md';
 
 /* 값을 받아와서 테이블 만들기 */
 
@@ -13,18 +13,26 @@ const Cards = styled.div`
         background-color : red;
     }
 
-
 `;
 
-function List({props}){
+const spanList = styled.span`
+    ${props => props.mainName !== 'true'
+     && css`
+        color:white;
+     `}
+`;
 
+
+function List({props}){
     return (
             <div class="list">
-                {props.list_name}
-                {
-                    props.Card.map(t=>(<Cards>{t.card_name}</Cards>))
-                }
-                <MdSchedule />
+                <spanList mainName="true">{props.list_name}</spanList>
+                <spanList>
+                    {
+                        props.Card.map(t=>(<Cards>{t.card_name}</Cards>))
+                    }
+                </spanList>                
+                <MdAdd /><spanList>add other card</spanList>
             </div>           
     );
 }
