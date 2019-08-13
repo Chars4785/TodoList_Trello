c53fd96d285a37ff899ecd06d41ccb60014916fb
asd <9105lgm@naver.com>
2019-08-13 16:48:08 +0900

    Axois test

--- postup/src/App.js
+++ postup/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {createGlobalStyle} from 'styled-components';
 import Header from './Components/Header';
 import TodoTemplate from './Components/TodoTemplate';
@@ -7,6 +7,10 @@ import './Components/Inform';
 import { Users } from './Components/Inform';
 import AddList from './Components/AddList';
 import Template from './Components/Template';
+import { Route } from 'react-router-dom';
+import { Login, About } from './pages';
+import axios from 'axios';
+
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -24,20 +28,58 @@ const MenuLine = styled.div`
     justify-content: center;
 
 `;
-    
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Header />
-      <MenuLine />
 
-      <Template>
-        <TodoTemplate user={Users}/>
-        <AddList />
-      </Template>
-    </>
-  );
+// axios.get('http://192.168.30.145:8080/user/findall')
+//   .then(function (response) {
+//     console.log(
+//       response.request
+
+//     );
+    
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log("데이터안왔어");
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
+
+
+
+class App extends Component {
+    render() {
+        return (
+          <>
+          <Header />
+                <Route exact path="/" component={Login}/>
+                <Route path="/about" component={About}/>
+          </>
+        );
+    }
 }
 
 export default App;
+
+
+
+/*
+
+<GlobalStyle />
+     <Header />
+          <MenuLine />
+          
+            
+          <Template>
+            <TodoTemplate user={Users}/>
+            <AddList />
+          </Template>
+
+
+          <>
+         
+          </>
+
+
+
+*/
