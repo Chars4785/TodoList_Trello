23315bb01cf80efd130d28eb8cfa48919e5354c8
Unknown <9105lgm@naver.com>
2019-08-21 12:53:58 +0900

    회원가입

--- postup/src/loginComponents/New.js
+++ postup/src/loginComponents/New.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {
+  LoginForm,LoginPageDiv,LoginInput,Button,Loading
+  ,Deny,StyledLink,HeadName,LabelName
+} from './Sass/LoginPageCss';
 
 
 function New(){
   return(
-    <div>
-      <input id="id" placeholder="아이디" type="text"></input><br />
-      <input id="pwd" placeholder="비밀번호" type="password"></input><br />
-      <input id="email" placeholder="Email"></input><br />
-    </div>
+    <LoginForm>
+      <LabelName>아이디</LabelName>
+      <LoginInput id="id" placeholder="아이디" type="text"></LoginInput>
+      <LabelName>비밀번호</LabelName>
+      <LoginInput id="pwd" placeholder="비밀번호" type="password"></LoginInput>
+      <LabelName>이메일</LabelName>
+      <LoginInput id="email" placeholder="Email"></LoginInput>
+      <Button>가입</Button>
+    </LoginForm>
   );
 }
-
 
 export default New;
