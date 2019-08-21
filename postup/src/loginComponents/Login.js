26fe155ad7f00fcba5723a3f340cec3f8ee90492
Unknown <9105lgm@naver.com>
2019-08-21 12:55:28 +0900

    Login Fail + Loading Animation

--- postup/src/loginComponents/Login.js
+++ postup/src/loginComponents/Login.js
@@ -1,24 +1,45 @@
 import React,{useState} from 'react';
 import LoginHook from '../Hooks/LoginHook';
-import './Sass/LoginScss.scss';
 import Users from '../Components/Inform';
 import styled,{css} from 'styled-components';
-import {MdAdd} from 'react-icons/md';
-import {CircleButton,InsertFormPositioner,InsertForm,Input, AddList} from './Sass/LoginCss';
+import { MdAdd } from 'react-icons/md';
+import Detail from '../Components/Detail';
+import {useBoardInform} from '../UserContext';
+import {
+  CircleButton,InsertFormPositioner,InsertForm,Input, AddList
+  ,BackGround,AddListInput,Board,CardList
+} from './Sass/LoginCss';
 
 function List({props}){
   const[open,setOpen] = useState(false);
+  const[detail,setDetail] = useState(false);
+  const[name,setName] = useState('');
   const onToggle = () => setOpen(!open);
+  
+  function onClick(props){ 
+    setName(props);
+    setDetail(!detail);
+  };
+
   return (
-    <>
-          <div className="List">
+    <>    {detail &&
+            <>
+            <BackGround onClick={onClick} />
+            <Detail props={name} />  
+            </>       
+          }
+          <CardList>
                 <div className="list_board"><b>{props.list_name}</b></div>
                   {
-                      props.Card.map(t=>(<div className ="card_list">{t.card_name}</div>))
+                      props.Card.map(t=>(
+                        <div className ="card_list" onClick={()=>onClick(t.card_name)} >
+                          {t.card_name}
+                        </div>
+                      ))
                   }
               {open && (
                   <InsertFormPositioner>
-                      <InsertForm>
+                      <InsertForm onSubmit={e=>e.preventDefault()}>
                           <Input placeholder="할 일을 입력 후,Enter"
                           autoFocus
                            />
@@ -28,20 +49,34 @@ function List({props}){
               <CircleButton onClick={onToggle} open={open}>
                 <MdAdd />
               </CircleButton> 
-          </div>   
+            </CardList>
       </>
   );
 }
 
 function Login(){
+  const[add,setAdd] = useState(false);
+  const list = useBoardInform();
+  const makeList = () =>{
+    setAdd(!add);
+  }
   return(
     <>
-      <div className="Board">
-        {Users.map(props =>(<List props={props}></List>))}
-        <AddList>
+      <Board className="Board">
+        {list.map(props =>(<List props={props}></List>))}
+        
+        
+        {
+          add &&
+          <>
+            <AddListInput type="text" placeholder="이름을 적어주세요"/>            
+            <MdAdd />
+          </>
+        }
+        <AddList onClick={makeList} add={add}>
           <MdAdd />
         </AddList>
-      </div>
+      </Board>
     </>
   );
 }
--- postup/src/loginComponents/LoginPage.js
+++ postup/src/loginComponents/LoginPage.js
@@ -1,49 +1,79 @@
-import  React,{useState,useContext,useRef} from 'react';
+import  React,{useState,useContext,} from 'react';
 import { Link } from 'react-router-dom';
 import {UserInformContext} from '../UserContext';
 import LoginHook from '../Hooks/LoginHook';
-import axios from 'axios';
-import {LoginForm,LoginPageDiv,LoginInput,Button} from './Sass/LoginPageCss';
+import {
+  LoginForm,LoginPageDiv,LoginInput,Button,Loading
+  ,Deny,StyledLink,HeadName,LabelName
+} from './Sass/LoginPageCss';
+import {Redirect} from 'react-router';
+import {UserSetContext} from '../UserContext';
 
-function LoginPage(){
-  const [user,userSet] = useState({
+function LoginPage(props){
+  const[delay,setDelay] = useState(false);
+  const[failLogin,setFailLogin] = useState(false);
+
+  const setUser = useContext(UserSetContext);
+
+  const [LoginUser,LoginUserSet] = useState({
     user_id:'',
     password:''
   });
   
-  const context = useContext(UserInformContext);
   const onChange = e =>{
     const{name,value} = e.target;
-    userSet({
-      ...user,
+    LoginUserSet({
+      ...LoginUser,
       [name]:value
     })
   }
-
+  /*
+  const result = await LoginHook('http://192.168.30.145:8080/user/login_user',LoginUser)
+     localhost:8080/mvcexam1/test
+    const result = await LoginHook('http://localhost:8080/mvcexam1/test',LoginUser)
+  */
   const onSubmit = async(e) =>{
     e.preventDefault();
-     const result = await LoginHook('http://192.168.30.145:8080/user/login_user',user)
-      .then(function(response){
-          console.log(response);
+    setDelay(true);
+    setFailLogin(false);
+
+    const result = await LoginHook('http://localhost:8080/mvcexam1/test',LoginUser)
+    .then(function(response){
+      //console.log(response);
+        setTimeout(function() {
+          setDelay(false);
+          props.history.push('/Login');
+        }, 2000);
       }).catch(function(erro){
+        setDelay(false);
+        setFailLogin(true);
           console.log("데이터오류")
       })
   }
-  
-
+  //<Loading />
 
   return(
     <>
-    <LoginForm onSubmit={onSubmit}>
+    
+    {delay ?  
+      <Loading />
+    :
+    <LoginForm onSubmit={onSubmit} >
+      <HeadName> Welcome to Project </HeadName>
+      {failLogin &&
+        <Deny><b>Invalid email or password.</b></Deny>
+      }
+      <LabelName>아이디</LabelName>
       <LoginInput name="user_id" type="text" placeholder="아이디" onChange={onChange}></LoginInput>
+      <LabelName>비밀번호</LabelName>
       <LoginInput name="password" type="password" placeholder="비밀번호" onChange={onChange}></LoginInput>
       <Button>로그인</Button>
-      <Link to ="/Login">로그인 연습</Link> 
-      <Link to="/New" className="nav-link">
-          회원가입
-      </Link>
+      
+      <StyledLink to="/New" > 회원가입 </StyledLink>
+      
+      <Link to="/Login">로그인 연습</Link> 
     </LoginForm>
-    
+    }
     </>
   );
 }
