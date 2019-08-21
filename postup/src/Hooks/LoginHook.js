91ad7c63f20e05d32f7ae519f0ad1a6a35a792a2
Unknown <9105lgm@naver.com>
2019-08-21 12:53:25 +0900

    Login reponse setting

--- postup/src/Hooks/LoginHook.js
+++ postup/src/Hooks/LoginHook.js
@@ -1,27 +1,28 @@
 import { useEffect, useState} from 'react';
 import axios from 'axios';
 
-const LoginHook = async (url,user)=>{
-  try{
-    return await axios.post(url,user)
-  }catch(e){
-    console.log(e);
-  }
+// const LoginHook = async (url,user)=>{
+//   try{
+//     return await axios.post(url,user);    
+//   }catch(e){
+//     console.log(e);
+//   }
+  
+// }
+async function LoginHook (url,user){
+  
+  const res = await axios.post(url,user)
+      .then(function (response) {
+        console.log("데이터왔어!!");
+
+      if(response.data.state === '0'){
+        throw "Fail";
+      }
+        return response;
+      });   
+  
+      return res;
 }
 
 export default LoginHook;
 
-// async function LoginHook (url,user){
-  
-//   const res = await axios.post(url,user)
-//       .then(function (response) {
-//         console.log("데이터왔어!!");
-//         return response;
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//         console.log("데이터 안옴");
-//       });   
-  
-//       return res;
-// }
