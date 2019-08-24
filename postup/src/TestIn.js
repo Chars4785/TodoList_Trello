51f09e9c48bf6aa3bd0ab64d98c53af82f6144c9
Unknown <9105lgm@naver.com>
2019-08-25 00:10:27 +0900

    Test Inform Json file

---
+++ postup/src/TestIn.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+
+export const tt =[
+    {
+        "list_id": 3,
+        "list_name": "LIST1",
+        "cardlist":[]
+    },
+    {
+        "list_id": 4,
+        "list_name": "LIST2_c2_cc2",
+        "cardlist": [
+          {
+              "card_name": "card1",
+              "card_description": "no",
+              "card_id": 5
+          },
+          {
+              "card_name": "card2",
+              "card_description": "no",
+              "card_id": 6,
+              "cardcontentlist": [
+                  {
+                      "cardcontent_id": 7,
+                      "cardcontent_name": "FUCK1",
+                      "cardcontent_content": "fucksdfdsf"
+                  },
+                  {
+                      "cardcontent_id": 8,
+                      "cardcontent_name": "FUCK2",
+                      "cardcontent_content": "fucksdfdsf"
+                  }
+              ]
+          }
+      ]
+  },
+  {
+      "list_id": 9,
+      "list_name": "LIST3_c3",
+      "cardlist": [
+          {
+              "card_name": "card1",
+              "card_description": "no",
+              "card_id": 10,
+              "cardcontentlist":[]
+          },
+          {
+              "card_name": "card2",
+              "card_description": "no",
+              "card_id": 11,
+              "cardcontentlist":[]
+
+          },
+          {
+              "card_name": "card3",
+              "card_description": "no",
+              "card_id": 12,
+              "cardcontentlist":[]
+          }
+      ]
+  }
+]
